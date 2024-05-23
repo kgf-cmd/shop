147c3c1127db9f07fea86b56712c5ac40f673979
kgf-cmd <Karolfracek123@gmail.com>
2024-05-23 14:44:07 +0200

    testy

---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+--https://www.javainuse.com/onlineBcrypt
+INSERT INTO local_user (email, first_name, last_name, password, username, email_verified)
+    VALUES ('UserA@junit.com', 'UserA-FirstName', 'UserA-LastName','$2a$10$tB2xQXVCWmK6C6YRg5PqLOWgs4lQUlkbYiJocNII2/EoHQJ1I5FTi', 'UserA', true)
+    ,('UserB@junit.com', 'UserB-FirstName', 'UserB-LastName', '$2a$10$WNJMzFDmeT0s5pCquQ3VF.z8Ub5n3O.UkrZDbz7s9JSzWO0tkvGIy','UserB', true)
